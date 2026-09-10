94ec0c25601cc7fce165a8c173f9633573a5f44e
Cade80-25 <charonito@gmail.com>
2026-09-10 14:37:53 -0300

    chore: limpiar scripts temporales de debug

--- inspect_userdata.cjs
+++
@@ -1,9 +0,0 @@
-const { app } = require('electron');
-
-app.whenReady().then(() => {
-  const userData = app.getPath('userData');
-  console.log('userData REAL:', userData);
-  console.log('app.name:', app.name);
-  console.log('app.getName():', app.getName());
-  app.quit();
-});
